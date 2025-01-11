c04990f38026767415b947635c6d13d836973435
Jakub Psarski <psarski@student.agh.edu.pl>
2025-01-11 02:19:12 +0100

    added vegetation variants

---
+++ src/main/java/proj/model/vegetation/AbstractVegetationVariant.java
@@ -0,0 +1,12 @@
+package proj.model.vegetation;
+
+import proj.util.Vector2d;
+
+import java.util.Random;
+
+public abstract class AbstractVegetationVariant {
+    private final Random random = new Random();
+
+    public boolean validatePlantPosition(Vector2d position) {return preferred(position) == (this.random.nextInt(5) != 4);}
+    abstract boolean preferred(Vector2d position);
+}
---
+++ src/main/java/proj/model/vegetation/ForestedEquator.java
@@ -0,0 +1,18 @@
+package proj.model.vegetation;
+
+import proj.util.Vector2d;
+
+public class ForestedEquator extends AbstractVegetationVariant{
+    private final Vector2d equatorLowerLeft;
+    private final Vector2d equatorUpperRight;
+
+    // constructor
+    public ForestedEquator(int equatorHeight, int mapWidth, int mapHeight) {
+        this.equatorLowerLeft = new Vector2d(0, (mapHeight - equatorHeight) / 2 + 1);
+        this.equatorUpperRight = new Vector2d(mapWidth - 1, (this.equatorLowerLeft.y() + equatorHeight - 1));
+    }
+
+    // utilities
+    @Override
+    public boolean preferred(Vector2d position) {return this.equatorLowerLeft.precedes(position) && this.equatorUpperRight.follows(position);}
+}
